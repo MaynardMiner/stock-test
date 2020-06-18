83a5e4d948c4e08432927fbaa04e5a2bf032f0bb
MaynardMiner <maynardviii@gmail.com>
2020-06-17 22:18:59 -0400

    Abstract 1

--- code/test1.ps1
+++ code/test1.ps1
@@ -5,4 +5,58 @@ Write-Host "We are going to try to analyze Apple"
 $AAPL = $Database.Stock_Table.AAPL
 
 
+<#
+
+ABSTRACT 1
+
+The Cash Conversion Cycle (CCC)
+The cash conversion cycle is a key indicator of the adequacy of a company's working capital position. 
+Working capital is the difference between a company's current assets, such as cash and current liabilities, 
+such as payables owed to suppliers for raw materials. Current assets and liabilities are short-term in nature, 
+meaning they're usually on the books for less than one year.
+
+The cash conversion cycle is an indicator of a company's ability to efficiently manage two 
+of its most important assets–accounts receivable and inventory. 
+Accounts receivable is the total money owed to a company by its 
+customers for booked sales.
+
+Components of the The Cash Conversion Cycle (CCC)
+Days sales outstanding is the average number of days it takes a company to collect payment from their customers
+after a sale is made. The cash conversion cycle uses days sales outstanding to help determine whether the company 
+is efficient at collecting from its clients.
+
+The cash conversion cycle calculation also calculates how long it takes a company to pay its bills. Days payables 
+outstanding represents the average number of days it takes a company to pay its suppliers and vendors.
+
+
+The third component of the CCC includes how long inventory sits idle. Days inventory outstanding is the 
+average number of days that inventory has been in stock before selling it.
+
+Calculated in days, the CCC reflects the time required to collect on sales and the time it takes to 
+turn over inventory. The cash conversion cycle calculation helps to determine how well a company is 
+collecting and paying its short-term cash transactions. If a company is slow to collect on its receivables, 
+for example, a cash shortfall could result and the company could have difficulty paying its bills and payables.
+
+The shorter the cycle, the better. Cash is king, and smart managers know that fast-moving working capital 
+is more profitable than unproductive working capital that is tied up in assets.
+
+Formula and Calculation of the Cash Conversion Cycle
+​	  
+CCC=DIO+DSO−DPO
+
+where:
+DIO=Days inventory outstanding
+DSO=Days sales outstanding
+DPO=Days payables outstanding
+
+There is no single optimal metric for the CCC, which is also referred to as a company's operating cycle. 
+As a rule, a company's CCC will be influenced heavily by the type of product or service it provides and 
+industry characteristics.
+
+Investors looking for investment quality in this area of a company's balance sheet must track the CCC 
+over an extended period of time (for example, five to 10 years) and compare its performance to that of 
+competitors. Consistency and decreases in the operating cycle are positive signals. Conversely, erratic 
+collection times and an increase in on-hand inventory are typically negative investment-quality indicators.
+#>
+
 
